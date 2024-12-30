c0a65b6a2ed90d79aea6e5f572c6fa03c6e39b27
Vishal.P <73173958+vishalpsj@users.noreply.github.com>
2024-12-30 21:04:27 +0530

    html, css and js files updated, everything is working perfectly, still no responsiveness, can be a checkpoint 1

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let searchCity = document.getElementById('searchCity')
 let reportBox = document.querySelector('.weather_report')
 let moreInfo = document.querySelector('.moreInfo')
 let city = document.querySelector('.weather_city')
-let date_time = document.querySelector('.date_time')
 let weather_forcast = document.querySelector('.weather_forcast')
 let weather_icon = document.querySelector('.weather_icon')
 let weather_temprature = document.querySelector('.weather_temprature')
@@ -22,58 +21,41 @@ const generateCountryName = (code) => {
     return new Intl.DisplayNames([code], { type: 'region' }).of(code);
 }
 
-// getting time 
-
-const generateDateTime = (dt) => {
-
-    const curDateTime = new Date(dt * 1000);
-
-    const options = {
-        weekdays: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-    }
-
-    const formatter = new Intl.DateTimeFormat('en-US', options);
-    return formatter.format(curDateTime);
+const getwindspeed = (s) => {
+    return s * 3.6
 }
 
 let theCityName = "new delhi"
 
 const getWeatherData = async () => {
     // api url
-    const weatherAPI_url = `https://api.openweathermap.org/data/2.5/weather?q=${theCityName}&units=metric&APPID=a5518e6d8640c09e4a18003b8e9d533d`;
+    const weatherAPI_url = `https://api.openweathermap.org/data/2.5/weather?q=${theCityName}&units=metric&appid=a5518e6d8640c09e4a18003b8e9d533d`;
+
+
     try {
         const res = await fetch(weatherAPI_url)
         const data = await res.json()
         console.log(data);
-        const { dt, main, name, sys, weather, wind } = data;
+        const { main, name, sys, weather, wind } = data;
 
         city.innerText = `${name}, ${generateCountryName(sys.country)}`
-        date_time.innerText = generateDateTime(dt)
         weather_forcast.innerHTML = weather[0].main
-        weather_temprature.innerHTML = `${main.temp.toFixed()}&deg;`
-        // weather_icon.innerHTML = weather[0].icon
+        weather_temprature.innerHTML = `${main.temp.toFixed()}&deg<sup>c</sup>`
         weather_icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png"></img>`
         min.innerHTML = `Min: ${main.temp_min.toFixed()}&deg;`;
         max.innerHTML = `Max: ${main.temp_max.toFixed()}&deg;`
         feelsLike.innerHTML = `${main.feels_like.toFixed()}&deg;`
         humidity.innerHTML = `${main.humidity}&percnt;`
-        windSpeed.innerHTML = `${wind.speed} m/s`
-        pressure.innerHTML = `${main.pressure} mbar`
-
-
+        const speedofWind = wind.speed
+        windSpeed.innerHTML = `${getwindspeed(speedofWind).toFixed(2)} km/h`
+        pressure.innerHTML = `${main.pressure} hPa`
 
     } catch (error) {
-        // console.log(error);
+        console.log(error);
         reportBox.style.display = "none"
         moreInfo.style.display = "none"
         document.querySelector('.errorMsgbox').style.display = "flex"
-        errorShowing.innerHTML = `We couldn't locate '${theCityName}', Try another.`
-
+        errorShowing.innerHTML = `We couldn't locate <b>'${theCityName}'</b>, Try another <i class="ri-emotion-unhappy-fill"></i>`
     }
 }
 
